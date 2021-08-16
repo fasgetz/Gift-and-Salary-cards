09e8f63c6d03c4c3557c16e991f8cd852ac902c7
Andrew Andrew <fasgetz@hotmail.com>
2021-08-16 18:28:13 +0300

    Добавление Identity БД

---
+++ Gift and Salary cards/Models/Identity/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gift_and_Salary_cards.Models.Identity
+{
+
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public class User : IdentityUser
+    {
+
+    }
+}
